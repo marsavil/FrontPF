c9f5ff59c2739b1031ce04a3dd03c92d19ac9184
marsavil <msvillaescusa@gmail.com>
2023-02-11 10:06:41 -0300

    importing react

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {appendErrors, useForm} from "react-hook-form";
 import { priceValidator } from "../validators/validators";
 import { Link } from "react-router-dom";
