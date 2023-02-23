3d4dd1d2d9371deed02aa01dc06ab2ab136bc19a
Mariano Villaescusa <msvillaescusa@gmail.com>
2023-02-23 10:26:39 -0300

    actualizacion de stock

--- src/PaymentRecieved/PaymentReceived.jsx
+++ src/PaymentRecieved/PaymentReceived.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React ,{useEffect} from "react";
 import { Link, useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { clearCarts } from "../Redux/actions";
 
 
   
   export const PaymentReceived = () =>{
 
+  const dispatch = useDispatch()
   const history = useHistory();
+
+  useEffect(() => {
+    dispatch(clearCarts());
+  }, [dispatch]);
+
   return(
     <div>
       <h1>Tu pago se ha acreditado correctamente</h1>
